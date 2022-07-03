82995a955fddc0cdefefff425ab7ee0dbddf7900
Brendon Malkis <brendonmalkis11@gmail.com>
2022-07-03 17:54:44 +0300

    Implemented the cell builder function that builds a single cell with content

--- View/Cell/CellBuilder.js
+++ View/Cell/CellBuilder.js
@@ -3,6 +3,8 @@ class CellBuilder
     buildCell(content)
     {
         let div = document.createElement("div");
+        div.classList.add("cell");
+        div.textContent = content;
         return div;
     }
 }
