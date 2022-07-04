5be1009557cfd1ea43ef02a8f34de08b39c114e9
Brendon Malkis <brendonmalkis11@gmail.com>
2022-07-04 14:44:09 +0300

    Moved routing logic to a different file

---
+++ View/Pages/Game/Game.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+
+    controllers.generateBoardController.getBoard(params.size);
+});
